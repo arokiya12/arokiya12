246d12892cd4d672fe7a083ebb09a5be15a60869
arokiya12 <arokiyavinnarasi@gmail.com>
2023-10-15 14:42:57 +0530

    Add files via upload
    day one assignment 14/09

---
+++ day1/Datatype.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Datatype {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int Entryage =26;
+		float milage  =33.4f;
+		 char letter ='A';
+		 double bankBalance = 2345.9999;
+		 String brandName = "Benz";
+		 System.out.println("Entryage is "+ Entryage  +"milage is " + milage + letter + bankBalance + brandName);
+		
+		 
+	}
+
+}
---
+++ day1/Ifstatement.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Ifstatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int Salary = 30000;
+		
+		if (Salary>20000) {
+			System.out.println("Salary is lower that 30K");
+		
+		}
+		else
+			System.out.println("salary is greater or equal to 30K");
+		}
+	}
